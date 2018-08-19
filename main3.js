8713a8405772713724aba9d31c8f51dadfdfba41
dingsheng <317010370@qq.com>
2018-08-19 22:32:44 +0800

    切换页面时 停止动画 防止动画紊乱

--- main3.js
+++ main3.js
@@ -1,17 +1,30 @@
-
-setInterval(()=>{
+function beginTimer(){
+    let timer = setInterval(()=>{
     let divs = $('#images>div')
     let len = divs.length;
     divs.each((i,e)=>{
-        let step = e.className.substr(4,1);
-        let className = 0;
-        if(step == 1){
-            className = 'page' + len;
-        }else if(step == len){
-            className = 'page' + (len-1);
-        }else{
-            className = 'page' + (step-1);
-        }
-        $(e).removeClass().addClass(className)
-    })
-},1000)
+            let step = e.className.substr(4,1);
+            let className = 0;
+            if(step == 1){
+                className = 'page' + len;
+            }else if(step == len){
+                className = 'page' + (len-1);
+            }else{
+                className = 'page' + (step-1);
+            }
+            $(e).removeClass().addClass(className)
+        })
+    },2000)
+    return timer
+}
+
+let timer = beginTimer();
+
+document.addEventListener('visibilitychange', function(e){
+    console.log(document.hidden)
+    if(document.hidden){
+        window.clearInterval(timer)
+    }else{
+        timer = beginTimer()
+    }
+}, false)
